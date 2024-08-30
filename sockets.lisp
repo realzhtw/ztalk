6dc95d6e5793c7e823a388d701bc2f9f039ffc18
Aleksej Lebedev <al@ztalk.org>
2024-08-30 16:00:30 +0200

    Less parentheses: let => with, let* => withs, let allows now only one binding.

--- sockets.lisp
+++ sockets.lisp
@@ -15,3 +15,17 @@
       (make-udp6-socket address port)
       (make-udp4-socket address port)))
 
+(defun bslice (buf start end)
+  (make-array (- end start)
+	      :element-type '(unsignedbyte 8)
+	      :displaced-to buf
+	      :displaced-index-offset start))
+
+(zdefun socket-receive (s buf start end)
+  (multiple-value-bind (b n addr)
+    (sb-bsd-sockets:socket-receive s (bslice buf start end) nil)
+    (list n addr)))
+
+(zdefun socket-send (s addr buf start end)
+  (sb-bsd-sockets:socket-send s (bslice buf start end) nil
+			      :address addr))
