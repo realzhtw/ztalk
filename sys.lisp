30d7ba166ebdf7e8354e681a91696725870a0344
Aleksej Lebedev <al@ztalk.org>
2024-09-05 09:37:28 +0200

    Changed names and some internals in padding (iomanip). Added read-bytes & write-bytes to sys.lisp.

--- sys.lisp
+++ sys.lisp
@@ -9,6 +9,12 @@
 (zdef sigstop sb-unix:SIGSTOP)
 (zdef sigcont sb-unix:SIGCONT)
 
+(zdefun read-bytes (fd buf start end)
+  (sb-unix:unix-read fd (sb-sys:sap+ (sb-sys:vector-sap buf) start) (- end start)))
+
+(zdefun write-bytes (fd buf start end)
+  (sb-unix:unix-write fd (sb-sys:sap+ (sb-sys:vector-sap buf) start) (- end start)))
+
 (zdefun cpu-time () (/ (get-internal-run-time) (float internal-time-units-per-second)))
 (zdefun real-time () (/ (get-internal-real-time) (float internal-time-units-per-second)))
 (zexport sleep (x))
